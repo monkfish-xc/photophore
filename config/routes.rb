9764978ad6ec9193482fb78f1c66e8b4928f4ec2
Kyle Cline <kyledcline@gmail.com>
2014-02-03 13:25:41 -0600

    Change public account path prefix to /p/

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Photophore::Application.routes.draw do
   get "static_pages/home"
 
-  get '/a/:public_hash', :to => 'accounts#show', :as => :account
+  get '/p/:public_hash', :to => 'accounts#show', :as => :account
 
-  # match '/a/:public_hash', to: 'accounts#show', via: 'get', as: :account
 end

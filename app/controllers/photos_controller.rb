95da9424fa4256ec62db2ca9cbf67043c4b1e5e1
Kyle Cline <kyledcline@gmail.com>
2014-02-04 16:00:06 -0600

    Remap routes and add api scope for ng-app

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,7 +5,7 @@ class PhotosController < ApplicationController
   end
 
   def create
-    @account = Account.create
+    @account = (photo_params[:account_hash].empty?) ? Account.create : Account.friendly.find(photo_params[:account_hash])
     @photo = @account.photos.build(photo_params)
     if @photo.save
       redirect_to @account
@@ -14,8 +14,12 @@ class PhotosController < ApplicationController
     end
   end
 
+  def show
+    render json: Photo.friendly.find(params[:public_hash])
+  end
+
   private
     def photo_params
-      params.require(:photo).permit!
+      params.require(:photo).permit(:account_hash, :image)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,29 @@
-# == Route Map (Updated 2014-02-03 17:19)
+# == Route Map (Updated 2014-02-04 15:52)
 #
-#      Prefix Verb URI Pattern               Controller#Action
-#     account GET  /p/:public_hash(.:format) accounts#show
-# new_account GET  /                         accounts#new
-#    accounts POST /accounts(.:format)       accounts#create
+#    Prefix Verb URI Pattern                   Controller#Action
+# new_photo GET  /                             photos#new
+#    photos POST /photos(.:format)             photos#create
+#   account GET  /p/:public_hash(.:format)     accounts#show
+#     photo GET  /i/:public_hash(.:format)     photos#show
+#           GET  /api/p/:public_hash(.:format) accounts#show
 #
 
 Photophore::Application.routes.draw do
   # get "static_pages/home"
 
   root :to => 'photos#new', :as => :new_photo
-
-  get '/p/:public_hash', :to => 'accounts#show', :as => :account
-  get '/i/:public_hash', :to => 'photos#show', :as => :photo
   resources :photos, only: :create
 
+  scope :p do
+    get '/:public_hash', :to => 'accounts#show', :as => :account
+  end
 
+  scope :i do
+    get '/:public_hash', :to => 'photos#show', :as => :photo
+  end
 
+  scope :api do
+    get '/p/:public_hash', :to => 'accounts#show'
+    get '/i/:public_hash', :to => 'photos#show'
+  end
 end

e249ce5dc46a1156e53a82d2fdbb6603c664a7a2
Kyle Cline <kyledcline@gmail.com>
2014-02-05 00:20:28 -0600

    Edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
-# == Route Map (Updated 2014-02-04 20:33)
+# == Route Map (Updated 2014-02-05 00:12)
 #
-#    Prefix Verb URI Pattern                   Controller#Action
-# new_photo GET  /                             photos#new
-#    photos POST /photos(.:format)             photos#create
-#   account GET  /p/:public_hash(.:format)     accounts#show
-#     photo GET  /i/:public_hash(.:format)     photos#show
-#           GET  /api/p/:public_hash(.:format) accounts#show
-#           GET  /api/i/:public_hash(.:format) photos#show
+#    Prefix Verb URI Pattern               Controller#Action
+# new_photo GET  /                         photos#new
+#    photos POST /photos(.:format)         photos#create
+#   account GET  /p/:public_hash(.:format) accounts#show
+#     photo GET  /i/:public_hash(.:format) photos#show
 #
 
 Photophore::Application.routes.draw do
@@ -15,11 +13,12 @@ Photophore::Application.routes.draw do
   root :to => 'photos#new', :as => :new_photo
   resources :photos, only: :create
 
+  scope :i do
+    get '/:public_hash', :to => 'photos#show', :as => :photo
+  end
+
   scope :p do
     get '/:public_hash', :to => 'accounts#show', :as => :account
   end
 
-  scope :i do
-    get '/:public_hash', :to => 'photos#show', :as => :photo
-  end
 end
